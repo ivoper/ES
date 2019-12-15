03478252e2855dbb850edad3a502601364481a37
Bernardo Costa <bjmcoosta97@gmail.com>
2019-12-15 02:25:04 +0000

    Atualizar

---
+++ GestorDeProjetosDeFinanciamento/GestorDeProjetosDeFinanciamento/apresentacao/geral/controlo/Projetos.cs
@@ -0,0 +1,26 @@
+﻿using GestorDeProjetosDeFinanciamento.apresentacao.geral.vista;
+using GestorDeProjetosDeFinanciamento.apresentacao.tecnico.controlo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeProjetosDeFinanciamento.apresentacao.geral.controlo
+{
+    class Projetos : Apresentador<FormListarProjetos, SimplesArgs>
+    {
+
+        public Projetos() : base(new FormListarProjetos())
+        {
+            Vista.Notificavel = this;
+            Vista.ShowDialog();
+        }
+
+        public override void Notificar(SimplesArgs args)
+        {
+
+        }
+
+    }
+}
--- GestorDeProjetosDeFinanciamento/GestorDeProjetosDeFinanciamento/apresentacao/geral/vista/FormListarProjetos.Designer.cs
+++ GestorDeProjetosDeFinanciamento/GestorDeProjetosDeFinanciamento/apresentacao/geral/vista/FormListarProjetos.Designer.cs
@@ -28,38 +28,40 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.projetos = new System.Windows.Forms.ListBox();
-			this.confirmar = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// projetos
-			// 
-			this.projetos.FormattingEnabled = true;
-			this.projetos.Location = new System.Drawing.Point(101, 12);
-			this.projetos.Name = "projetos";
-			this.projetos.Size = new System.Drawing.Size(486, 264);
-			this.projetos.TabIndex = 0;
-			// 
-			// confirmar
-			// 
-			this.confirmar.Location = new System.Drawing.Point(274, 315);
-			this.confirmar.Name = "confirmar";
-			this.confirmar.Size = new System.Drawing.Size(149, 64);
-			this.confirmar.TabIndex = 1;
-			this.confirmar.Text = "Confirmar";
-			this.confirmar.UseVisualStyleBackColor = true;
-			// 
-			// FormListarProjetos
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(693, 407);
-			this.Controls.Add(this.confirmar);
-			this.Controls.Add(this.projetos);
-			this.Name = "FormListarProjetos";
-			this.Text = "Gestor de Projetos de Financiamento";
-			this.Load += new System.EventHandler(this.FormListarProjetos_Load);
-			this.ResumeLayout(false);
+            this.projetos = new System.Windows.Forms.ListBox();
+            this.confirmar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // projetos
+            // 
+            this.projetos.FormattingEnabled = true;
+            this.projetos.Location = new System.Drawing.Point(12, 12);
+            this.projetos.Name = "projetos";
+            this.projetos.Size = new System.Drawing.Size(669, 316);
+            this.projetos.TabIndex = 0;
+            this.projetos.SelectedIndexChanged += new System.EventHandler(this.projetos_SelectedIndexChanged);
+            // 
+            // confirmar
+            // 
+            this.confirmar.Location = new System.Drawing.Point(275, 334);
+            this.confirmar.Name = "confirmar";
+            this.confirmar.Size = new System.Drawing.Size(149, 64);
+            this.confirmar.TabIndex = 1;
+            this.confirmar.Text = "Confirmar";
+            this.confirmar.UseVisualStyleBackColor = true;
+            this.confirmar.Click += new System.EventHandler(this.confirmar_Click);
+            // 
+            // FormListarProjetos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 407);
+            this.Controls.Add(this.confirmar);
+            this.Controls.Add(this.projetos);
+            this.Name = "FormListarProjetos";
+            this.Text = "Gestor de Projetos de Financiamento";
+            this.Load += new System.EventHandler(this.FormListarProjetos_Load);
+            this.ResumeLayout(false);
 
 		}
 
