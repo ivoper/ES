eede6a1e15ecbf1dbdc28065ac0cd70c2e640357
DuarteFelicio <duartefelicio1@hotmail.com>
2020-01-06 19:11:59 +0000

    fodasse tou cheio deste projeto

--- GestorDeProjetosDeFinanciamento/GestorDeProjetosDeFinanciamento/apresentacao/gestor_de_financiamento/controlo/ReforcoDeFinanciamento.cs
+++ GestorDeProjetosDeFinanciamento/GestorDeProjetosDeFinanciamento/apresentacao/gestor_de_financiamento/controlo/ReforcoDeFinanciamento.cs
@@ -41,10 +41,7 @@ namespace GestorDeProjetosDeFinanciamento.apresentacao.gestor_de_financiamento.c
                 return;
             }
 
-            // Confirmacao da data do despacho de um projeto de bonificacao, se ja expirou ou nao.
             Despacho ultimoDespacho = servicoDespacho.LerUltimoDespacho(projeto);
-            if (ultimoDespacho.DespachoBonificacao != null)
-                ConfirmarDespachoExpirado(projeto, ultimoDespacho.DespachoBonificacao);
 
             servicoPedidoDeReforco.CriarPedidoDeReforco(new PedidoDeReforco()
             {
@@ -71,20 +68,6 @@ namespace GestorDeProjetosDeFinanciamento.apresentacao.gestor_de_financiamento.c
             Vista.Hide();
 			Vista.Close();
 		}
-
-        private void ConfirmarDespachoExpirado(Projeto projeto, DespachoBonificacao despachoBonificacao)
-        {
-            DateTime dataLimite = despachoBonificacao.periodo.GetValueOrDefault();
-            if (DateTime.Compare(DateTime.Today, dataLimite) > 0)
-            {
-                String estado = servicoObterEstados.ObterEstado(projeto.estado).estado1;
-                EstadosProjeto estadoAntigo = Utils.StringParaEstado(estado);
-                EstadosProjeto estadoNovo = MaquinaDeEstados.processar(estadoAntigo, EventosProjeto.pagamento_completo);
-                String superEstadoNovo = Utils.EstadoParaString(estadoNovo);
-                projeto.estado = servicoObterEstados.ObterIdEstado(superEstadoNovo);
-                servicoProjetos.AtualizarProjeto(projeto);
-            }
-        }
 
         private bool VerificarArgumentos(ReforcoDeFinanciamentoArgs args)
         {
